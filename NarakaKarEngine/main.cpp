d0339da75a1b9e5165ccd8be46e0ea39548fbe63
Rabin Gajmer <rabingajmer_189@hotmail.com>
2022-07-28 01:51:12 +0900

    #7[engineUi] Added a simple example window of DearImgui.

--- NarakaKarEngine/main.cpp
+++ NarakaKarEngine/main.cpp
@@ -1,18 +1,22 @@
 #include "pch.h"
 #include "RenderEngineMain.h"
+#include "EngineUICreator.h"
 
 using namespace NarakaKarEngine;
 using namespace RenderEngine;
+using namespace EngineUI;
 
 int main()
 {
 	std::unique_ptr<RenderEngineMain> engine = std::make_unique<RenderEngineMain>();
-	
-	engine->Init();
+	std::unique_ptr<EngineUICreator> engineUI = std::make_unique<EngineUICreator>(engine->Init(), true, "#version 460");
 
 	do 
 	{
-		engine->Update();
+		//get + handle user input events
+		glfwPollEvents();
+		engineUI->Update();
+		engine->Update([&]{ engineUI->Render(); });
 	} while (!engine->IsEnd());
 
 	return 0;
