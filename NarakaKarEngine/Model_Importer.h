f8a26a6ff1eef1974e5d627820f210239ececfa6
Rabin Gajmer <rabingajmer_189@hotmail.com>
2022-12-31 18:32:25 +0900

    #57[graphic lib] Created a basis class for importing model data.

---
+++ NarakaKarEngine/Model_Importer.h
@@ -0,0 +1,53 @@
+#ifndef MODEL_IMPORTER
+#define MODEL_IMPORTER
+
+#include "pch.h"
+
+namespace NarakaKarEngine
+{
+	namespace RenderEngine
+	{
+		struct BoneMatrix
+		{
+			aiMatrix4x4 offset_matrix;
+			aiMatrix4x4 final_world_transform;
+		};
+
+		struct KeyFrames
+		{
+			GLfloat value;
+			GLfloat time;
+		};
+
+		//struct AnimationNode
+		//{
+		//	std::string NodeName;
+		//	std::string KeyFrames;
+		//};
+
+		class Mesh;
+
+		class Model_Importer final
+		{
+		public:
+			explicit Model_Importer() = delete;
+
+			void LoadModel(std::string&& fileName);
+
+			~Model_Importer();
+
+			std::unique_ptr<std::vector<std::weak_ptr<Mesh>>> MeshList;
+			std::unique_ptr<std::map<TexType, std::vector<std::weak_ptr<Texture>>>> TextureMap;
+			std::unique_ptr<std::vector<aiAnimation>> AnimationList;
+			std::unique_ptr< std::vector<aiNode>> NodeHierarchy;
+		private:
+			struct Impl;
+
+			const Impl* Pimpl() const { return m_pImpl.get(); }
+			Impl* Pimpl() { return m_pImpl.get(); }
+
+			std::unique_ptr<Impl> m_pImpl;
+		};
+	}
+}
+#endif
