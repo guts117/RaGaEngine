fe93fd86cf923820510e3a39df5ab6da9efa0f45
Rabin Gajmer <rabingajmer_189@hotmail.com>
2023-05-05 19:00:43 +0900

    #68[pimpl] Using ForwardDeclaredPimpl for Render_Object.

--- RenderEngine/Render_Object.h
+++ RenderEngine/Render_Object.h
@@ -30,35 +30,39 @@ namespace NarakaRenderEngine
 		public:
 			explicit Render_Object() = delete;
 
-			explicit Render_Object(std::unique_ptr<std::vector<std::weak_ptr<Mesh>>>&& meshes
-								, std::unique_ptr<std::map<TexType, std::vector<std::weak_ptr<Texture>>>> textureMap = nullptr
-								, std::shared_ptr<glm::mat4> modelMatrix = nullptr
-								, std::shared_ptr<glm::mat4> prevModelMatrix = nullptr
-								, std::unique_ptr<std::vector<std::weak_ptr<BoneTransform>>>&& boneMatrices = nullptr);
+			explicit Render_Object(std::vector<Mesh*>&& meshes
+								, std::map<TexType, std::vector<Texture*>>&& textureMap = std::map<TexType, std::vector<Texture*>>()
+								, glm::mat4* modelMatrix = nullptr
+								, glm::mat4* prevModelMatrix = nullptr
+								, std::vector<BoneTransform*>&& boneMatrices = std::vector<BoneTransform*>());
 
-			void SetTextures(std::map<TexType, std::vector<std::weak_ptr<Texture>>>&& textureMap);
+			void SetTextures(std::map<TexType, std::vector<Texture*>>&& textureMap);
 
 			void ResetTextures();
-			const std::weak_ptr<glm::mat4>& GetModelMatrix() const ;
+			const glm::mat4* GetModelMatrix() const ;
 
-			Render_Object(Render_Object&& rhs) noexcept = default;
-			Render_Object& operator=(Render_Object&& rhs) noexcept = default;
+			Render_Object(Render_Object&& rhs) noexcept;
+			Render_Object& operator=(Render_Object&& rhs) noexcept;
 
 			Render_Object(const Render_Object& rhs) noexcept = delete;
 			Render_Object& operator=(const Render_Object& rhs) noexcept = delete;
 
-			void RenderObject(Shader_Object& shader, RenderObjectParams&& params);
+			void RenderObject(Shader_Object& shader, RenderObjectParams&& params) const;
 
 			const bool IsTesselated() const;
 
-			~Render_Object();
+			~Render_Object() noexcept;
 		private:
 			struct Impl;
 
-			const Impl* Pimpl() const { return m_pImpl.get(); }
-			Impl* Pimpl() { return m_pImpl.get(); }
+			const Impl& Pimpl() const { return m_pImpl.Get(); }
+			Impl& Pimpl() { return m_pImpl.Get(); }
 
-			std::unique_ptr<Impl> m_pImpl;
+#ifdef NDEBUG //size of map debug(24), release(16) and size of vector debug(32), release(24)
+			ForwardDeclaredPimpl<Impl, alignof(void*) * 10, alignof(void*)> m_pImpl;
+#else
+			ForwardDeclaredPimpl<Impl, alignof(void*) * 13, alignof(void*)> m_pImpl;
+#endif;
 		};
 	}
 }
