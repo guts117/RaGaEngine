27ed35137cc4597cd16798dec9e658ac2eba5087
Rabin Gajmer <rabingajmer_189@hotmail.com>
2023-05-24 12:28:28 +0900

    #81[test] Testing exuceution time of ClusteringMemoryPool on ConsoleBasedTest project.

--- ConsoleBasedTest/ConsoleBasedTest.cpp
+++ ConsoleBasedTest/ConsoleBasedTest.cpp
@@ -9,6 +9,7 @@
 #include <algorithm>
 #include <iterator>
 #include <iostream>
+#include <chrono>
 
 using namespace std;
 
@@ -18,16 +19,11 @@ struct NameLog
     string motherName;
     string fatherName;
 
-    void ChangeName()
+    void ChangeName(string _name, string _motherName, string _fatherName)
     {
-    }
-
-    void ChangeMotherName()
-    {
-    }
-
-    void ChangeFatherName()
-    {
+        name = _name;
+        motherName = _motherName;
+        fatherName = _fatherName;
     }
 };
 
@@ -37,26 +33,35 @@ struct AgeLog
     int motherAge;
     int fatherAge;
 
-    void ChangeAge()
+    void ChangeAge(int _age, int _motherAge, int _fatherAge)
     {
+        age = _age;
+        motherAge = _motherAge;
+        fatherAge = _fatherAge;
     }
+};
 
-    void ChangeMotherAge()
-    {
-    }
-
-    void ChangeFatherAge()
-    {
-    }
+struct PersonLog
+{
+    rw_clustering_ptr<NameLog> nameLog;
+    rw_clustering_ptr<AgeLog> ageLog;
 };
 
 struct PersonHandler
 {
-    vector<rw_clustering_ptr<NameLog>> nameLogs;
-    vector<rw_clustering_ptr<AgeLog>> ageLogs;
+    vector<PersonLog> personLogs;
 
     void Update()
     {
+        std::random_device rd;
+        std::mt19937 g(rd());
+
+        for (auto& pL : personLogs)
+        {
+            auto id = g();
+            pL.nameLog.write(&NameLog::ChangeName, "rabin" + to_string(id), "rabin mom" + to_string(id), "rabin dad" + to_string(id));
+            pL.ageLog.write(&AgeLog::ChangeAge, 25 + id, 42 + id, 45 + id);
+        }
     }
 };
 
@@ -64,23 +69,50 @@ int main()
 {
     ClusteringMemoryPool<NameLog> nameLogPool = ClusteringMemoryPool<NameLog>(5);
     ClusteringMemoryPool<AgeLog> ageLogPool = ClusteringMemoryPool<AgeLog>(5);
-    vector<PersonHandler> personHandler = std::vector<PersonHandler>();
+    vector<PersonHandler> personHandlers = std::vector<PersonHandler>();
 
-    for(int i = 0; i < 1000; ++i)
+    for(int a = 0; a < 10; ++a)
     {
-        auto id = to_string(i);
-        auto person = PersonHandler();
-        person.nameLogs.push_back(nameLogPool.AddToPool(NameLog{ "rabin"+ id,  "rabin mom"+ id, "rabin dad"+ id }));
-        person.ageLogs.push_back(ageLogPool.AddToPool(AgeLog{ 25 + i, 42 + i, 45 + i}));
-        personHandler.push_back(std::move(person));
+        auto personHandlr = PersonHandler();
+
+        for (int i = 0; i < 1000; ++i)
+        {
+            auto id = to_string(i);
+            auto personLog = PersonLog{ nameLogPool.AddToPool(NameLog{ "rabin" + id,  "rabin mom" + id, "rabin dad" + id })
+                                , ageLogPool.AddToPool(AgeLog{ 25 + i, 42 + i, 45 + i }) };
+            personHandlr.personLogs.push_back(std::move(personLog));
+        }
+
+        personHandlers.push_back(std::move(personHandlr));
     }
 
     std::random_device rd;
     std::mt19937 g(rd());
 
-    std::shuffle(personHandler.begin(), personHandler.end(), g);
+    std::shuffle(personHandlers.begin(), personHandlers.end(), g);
+    for(int i = 0; i < personHandlers.size(); ++i)
+    {
+        std::shuffle(personHandlers[i].personLogs.begin(), personHandlers[i].personLogs.end(), g);
+    }
 
-    std::cout << "Hello World!\n";
+    auto start = chrono::high_resolution_clock::now();
+
+    for (auto& pH : personHandlers) 
+    {
+        pH.Update();
+    }
+
+    nameLogPool.ExecuteClusteredTasks();
+    ageLogPool.ExecuteClusteredTasks();
+
+    auto end = chrono::high_resolution_clock::now();
+
+    // Calculating total time taken by the program.
+    double time_taken = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+    time_taken *= 1e-9;
+
+    cout << "Time taken by program is : " << fixed << time_taken << setprecision(9);
+    cout << " sec" << endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
